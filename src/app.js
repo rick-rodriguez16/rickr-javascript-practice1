5c963532179d3dea5c683dcc214fefce2348d174
Rick Rodriguez <rickrod6502@gmail.com>
2026-02-14 01:40:29 +0000

    nested for example

--- src/app.js
+++ src/app.js
@@ -6,52 +6,21 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-   
-// create 4 arrays that are part of a sentence
 
-// excuse  the dog   ate        my homework   before class
-//         who       action     what          when
+    // create combinations of strings using more than one array
 
-let who = ['the dog', 'my grandma'];
-let action = ['ate', 'broke'];
-let what = ['my phone', 'my car'];
-let when = ['this morning.', 'while I was sleeping.'];
-let className = ["alert-warning", "alert-success"]
+    const colors = ["Black", "Red", "White", "Green", "Pink"];
+    const animals = ["Wolf", "Chicken", "Panther", "Crocodile"];
+    const locations = ["swamp", "tundra", "jungle", "desert"];
 
-// create a variable that holds a random number between 0 - 1
-// print a random element from the who array
-
-// let randomIndex = Math.floor(Math.random() * 2);
-// let selectedWho = who[randomIndex]
-
-// randomIndex = Math.floor(Math.random() * 2);
-// let selectedAction = action[randomIndex]
-
-// randomIndex = Math.floor(Math.random() * 2);
-// let selectedWhat = what[randomIndex];
-
-// randomIndex = Math.floor(Math.random() * 2);
-// let selectedWhen = when[randomIndex];
-
-function selectExcuse(array) {
-
-  let randomIndex = Math.floor(Math.random() * array.length);
-  return array[randomIndex];
-}
-
-let selectedWho = selectExcuse(who);
-let selectedAction = selectExcuse(action);
-let selectedWhat = selectExcuse(what);
-let selectedWhen = selectExcuse(when);
+    for (let colorIndex = 0; colorIndex < colors.length; colorIndex++) {
+      for (let animalIndex = 0; animalIndex < animals.length; animalIndex++) {
+        for (let locationIndex = 0; locationIndex < locations.length; locationIndex++) {
+          console.log(colors[colorIndex] + " " + animals[animalIndex] + " " + locations[locationIndex] + "!");
+        }
+      }
+    }
 
 
-// render the excuse to the browser
-let htmlElement = document.querySelector(".excuse");
-
-let selectedClass = selectExcuse(className);
-htmlElement.classList.add(selectedClass)
-htmlElement.innerHTML = selectedWho + " " + selectedAction + " " + selectedWhat + " " + selectedWhen;
-
-
-
+  
 };
